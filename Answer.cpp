c6faf4b2888a6fe017bfe5b93211e04ca240f203
なつき <natsuki@knct.info>
2011-08-22 16:44:49 +0900

    クラスAnswerのAddとOutputを実装

--- Answer.cpp
+++ Answer.cpp
@@ -18,10 +18,58 @@ Answer::~Answer()
 
 void Answer::Add(long x, long y, long n)
 {
-	// TODO:
+	struct stamp temp = { x, y, n };
+	stamps.push_back(temp);
 }
 
 void Answer::Output(boost::shared_ptr<std::ostream> ofs)
 {
-	// TODO:
+	std::sort(stamps.begin(), stamps.end());
+	std::vector<stamp> temp;
+	temp.reserve(stamps.size());
+
+	// ダブり消去
+	stamp pre = { -1, -1, -1 };
+	int c = 0;
+	BOOST_FOREACH(auto &stamp, stamps)
+	{
+		c++;
+		if(pre == stamp){
+		}else{
+			if(pre.n != -1 && c%2 == 1){
+				temp.push_back(pre);
+			}
+			c = 0;
+			pre = stamp;
+		}
+	}
+	c++;
+	if(pre.n != -1 && c%2 == 1){
+		temp.push_back(pre);
+	}
+
+	(*ofs) << temp.size() << std::endl;
+	BOOST_FOREACH(auto &stamp, temp)
+	{
+		(*ofs)<< stamp.n  << stamp.x << " " << stamp.y << " " << std::endl;
+	}
+}
+
+bool operator<(const Answer::stamp &a, const Answer::stamp &b)
+{
+	if(a.x < b.x) return (true);
+	else return (false);
+	if(a.y < b.y) return (true);
+	else return (false);
+	if(a.n < b.n) return (true);
+	else return (false);
+	return (false);
+}
+
+bool operator==(const Answer::stamp &a, const Answer::stamp &b)
+{
+	if(a.x != b.x) return (false);
+	if(a.y != b.y) return (false);
+	if(a.n != b.n) return (false);
+	return (true);
 }
--- Answer.h
+++ Answer.h
@@ -15,40 +15,18 @@ public:
 	~Answer();
 	void Add(long x, long y, long n);
 	void Output(boost::shared_ptr<std::ostream> ofs);
-private:
+public:
 	struct stamp
 	{
 		long x;
 		long y;
 		long n;
-		bool operator<(const struct stamp &s)
-		{
-			if (x < s.x)
-				return (true);
-			else
-				return (false);
-			if (y < s.y)
-				return (true);
-			else
-				return (false);
-			if (n < s.n)
-				return (true);
-			else
-				return (false);
-			return (false);
-		}
-		bool operator==(const struct stamp &s)
-		{
-			if (x != s.x)
-				return (false);
-			if (y != s.y)
-				return (false);
-			if (n != s.n)
-				return (false);
-			return (true);
-		}
 	};
+private:
 	std::vector<struct stamp> stamps;
 };
+
+bool operator<(const Answer::stamp &a, const Answer::stamp &b);
+bool operator==(const Answer::stamp &a, const Answer::stamp &b);
 
 #endif /* ANSWER_H_ */
