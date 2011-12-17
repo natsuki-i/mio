82f5a8c1cedd9db05ff63cea455e3292fffa2ec9
なつき <natsuki@knct.info>
2011-12-17 09:51:03 +0900

    AlgorithmFillOneはシングルスレッドのほうがちょっと速い

--- AlgorithmFillOne.cpp
+++ AlgorithmFillOne.cpp
@@ -25,21 +25,8 @@ AlgorithmFillOne::~AlgorithmFillOne()
 void AlgorithmFillOne::Solve(Problem &problem, Answer &answer)
 {
 	long int h = problem.image.getHeight();
-	worker w1(problem, answer, 0, h/4);
-	worker w2(problem, answer, h/4, h/2);
-	worker w3(problem, answer, h/2, 3*h/4);
-	worker w4(problem, answer, 3*h/4, h);
-	std::thread t1(w1), t2(w2), t3(w3), t4(w4);
-	t1.join();
-	t2.join();
-	t3.join();
-	t4.join();
-}
-
-void AlgorithmFillOne::worker::operator()()
-{
 	long int w = problem.image.getWidth();
-	for(int y = sy;y < ey;y++){
+	for(int y = 0;y < h;y++){
 		for(int x = 0;x < w;x++){
 			if(problem.image.get(x, y)){
 				problem.AffixStamp(x, y, problem.stamps_size[0].getNumber());
@@ -48,3 +35,4 @@ void AlgorithmFillOne::worker::operator()()
 		}
 	}
 }
+
